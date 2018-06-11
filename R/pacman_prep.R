8150f1cedabe417d8c665e5a5397e60314e2c833
lihualei71 <lihua.lei@berkeley.edu>
2018-06-10 23:48:27 -0700

    README

--- R/pacman_prep.R
+++ R/pacman_prep.R
@@ -1,49 +1,94 @@
-library("dplyr")
-select <- dplyr::select
 
-data <- read.csv("../data/pacman.csv",
-                 stringsAsFactors=FALSE) %>%
-    filter(inrct == 1) %>%
-    select(-ahsurv, -tc, -nb20, -nb30,
-           -contains("miss"), -contains("cat"),
-           -rate, -shanew,
-           -bookletnumber, -unit, -icnno, -inrct,
-           -innrsoverlapcent, -centinboth,
-           -daicu, -timemonths, -timecat, -year,
-           -system, -admtype, -inttemprct,
-           -med, -inttemprct7, -inttemprct12,
-           -IMprobcheck, -ka, -r, -kr,
-           -infect24rct, -labinfect, -susinfect,
-           -AP2probUK,
-           -AP2aps, -benewrct3, -AP2score, -IMprobrctnew
-           ) %>%
-    mutate(sex = ifelse(sex == "F", 1, 0))
+R version 3.3.3 (2017-03-06) -- "Another Canoe"
+Copyright (C) 2017 The R Foundation for Statistical Computing
+Platform: x86_64-apple-darwin13.4.0 (64-bit)
 
-data1 <- filter(data, pac == 1) %>% select(-pac)
-data0 <- filter(data, pac == 0) %>% select(-pac)
-y1 <- data1$new_qals
-y0 <- data0$new_qals
-n1 <- nrow(data1)
-n0 <- nrow(data0)
-n <- n1 + n0
+R is free software and comes with ABSOLUTELY NO WARRANTY.
+You are welcome to redistribute it under certain conditions.
+Type 'license()' or 'licence()' for distribution details.
 
-X <- rbind(data1, data0) %>% select(-new_qals)
-formula <- paste(names(X), collapse = "+")
-formula <- paste0("~ . + (agem + sex) * (", formula, ")")
-X <- model.matrix(as.formula(formula), data = X)[, -1]
+  Natural language support but running in an English locale
 
-## Preparation to simulate potential outcomes
-treat <- ifelse(1:n <= n1, TRUE, FALSE)
-Yobs <- c(y1, y0)
-data <- cbind(data.frame(Y = Yobs), as.data.frame(X))
+R is a collaborative project with many contributors.
+Type 'contributors()' for more information and
+'citation()' on how to cite R or R packages in publications.
 
-mod1 <- lm(Y ~ ., data = data, subset = treat)
-sd1 <- sd(resid(mod1)) / 2
-Y1root <- predict(mod1, newdata = data) %>% as.numeric
-mod0 <- lm(Y ~ ., data = data, subset = !treat)
-sd0 <- sd(resid(mod0)) / 2
-Y0root <- predict(mod0, newdata = data) %>% as.numeric
+Type 'demo()' for some demos, 'help()' for on-line help, or
+'help.start()' for an HTML browser interface to help.
+Type 'q()' to quit R.
 
-data <- list(Y1 = Y1root, Y0 = Y0root, X = X, pi1 = n1 / n,
-             sd1 = sd1, sd0 = sd0)
-save(file = "../data/pacman_simul.RData", data)
+[Previously saved workspace restored]
+
+> library("dplyr")
+
+Attaching package: ‘dplyr’
+
+The following objects are masked from ‘package:stats’:
+
+    filter, lag
+
+The following objects are masked from ‘package:base’:
+
+    intersect, setdiff, setequal, union
+
+> select <- dplyr::select
+> 
+> library("Matching")
+Loading required package: MASS
+
+Attaching package: ‘MASS’
+
+The following object is masked _by_ ‘.GlobalEnv’:
+
+    select
+
+The following object is masked from ‘package:dplyr’:
+
+    select
+
+## 
+##  Matching (Version 4.9-2, Build Date: 2015-12-25)
+##  See http://sekhon.berkeley.edu/matching for additional documentation.
+##  Please cite software as:
+##   Jasjeet S. Sekhon. 2011. ``Multivariate and Propensity Score Matching
+##   Software with Automated Balance Optimization: The Matching package for R.''
+##   Journal of Statistical Software, 42(7): 1-52. 
+##
+
+> data(lalonde)
+> set.seed(1)
+> 
+> Yobs <- lalonde$re78
+> T <- lalonde$treat
+> X <- lalonde %>% select(-re78, -treat)
+> X <- model.matrix(~ .^2, data = X) %>% as.data.frame %>%
++     select(-`(Intercept)`, -`black:hisp`,
++            -`re74:u74`, -`re75:u75`,
++            -`re74:re75`, -`re74:u75`,
++            -`re75:u74`) %>% as.matrix
+> 
+> ## Preparation to simulate potential outcomes
+> treat <- as.logical(T)
+> Y1 <- Y0 <- Yobs
+> n1 <- sum(treat)
+> n0 <- sum(!treat)
+> n <- n1 + n0
+> Y1[!treat] <- NA
+> Y0[treat] <- NA
+> data <- cbind(data.frame(Y = Yobs), as.data.frame(X))
+> 
+> mod1 <- lm(Y ~ ., data = data, subset = treat)
+> sd1 <- sd(resid(mod1)) / 2
+> Y1root <- predict(mod1, newdata = data) %>% as.numeric
+> 
+> mod0 <- lm(Y ~ ., data = data, subset = !treat)
+> sd0 <- sd(resid(mod0)) / 2
+> Y0root <- predict(mod0, newdata = data) %>% as.numeric
+> 
+> data <- list(Y1 = Y1root, Y0 = Y0root, X = X, pi1 = n1 / n,
++              sd1 = sd1, sd0 = sd0)
+> save(file = "../data/lalonde_simul.RData", data)
+> 
+> proc.time()
+   user  system elapsed 
+  0.630   0.073   0.786 
--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 This repository contains all R code to replicate the results and the figures in our paper: [Regression-adjusted Estimators in Randomized Experiments With A Diverging Number of Covariates](https://arxiv.org/abs/). 
 
 ## Introduction of R files 
+The folder `R/` contains all R files: 
 
 - `ols_adj.R` gives the function to estimate un-debiased and debiased estimators as well as four types of variance estimators;
 - `expr_fun.R` gives the framework of an experiment, which returns the bias ratio, standard deviation inflation ratios, 95% coverage, skewness, kurtosis and p-values from Shapiro-Wilks tests;
@@ -11,7 +12,32 @@ This repository contains all R code to replicate the results and the figures in 
 - `asym_simul_plot.R`, `synthetic_simul_plot.R`, `real_simul_plot.R`, `lscores_plot.R` make all plots
 - `lalonde_prep.R`, `pacman_prep.R`, `star_prep.R` generates the covariate matrices and the potential outcomes from real datasets.
 
+
+## Replicating the Experiments
 The folder `bash/` contains all files that facilitate job submission to the cluster. 
 
-The folder `data/`
-## Replicating the Experiments
+- `gen_jobs_asym_simul.sh`, `gen_jobs_synthetic_simul.sh`, `gen_jobs_real_simul.sh` generates separate jobs to each node in the cluster. The number of jobs are 243, 600 and 600, respectively, which equal to the number of rows in `params_asym_simul.txt`, `params_synthetic_simul.txt` and `params_real_simul.txt`. Our cluster takes job submission via `sbatch` command. For other system, one can replace sbatch in each `.sh` file by other appropriate command. 
+
+- `params_asym_simul.txt`, `params_synthetic_simul.txt`, `params_real_simul.txt` gives all experimental settings. 
+
+- Each job will generate a `.sh` file and a `.Rout` file into the folder `results/`. One can monitor the process by checking `.Rout` file.
+
+- Each job will save output as a `.RData` file into the folder `data/`. `asym_simul_post.R`, `synthetic_simul_post.R`, `real_simul_post.R` will marge them.
+
+## Plots 
+For those who are not willing to replicate all results, we provide the merged results `asym_simul_summary.RData`, `synthetic_simul_summary.RData` and `real_simul_summary.RData` in the folder `data/`, which are ready for making all plots. The figures will be generated in the folder `figs/` by running the following commands
+
+```
+cd R
+R CMD BATCH --no-save lalonde_prep.R
+R CMD BATCH --no-save pacman_prep.R
+R CMD BATCH --no-save star_prep.R
+R CMD BATCH --no-save asym_simul_plot.R
+R CMD BATCH --no-save synthetic_simul_plot.R
+R CMD BATCH --no-save real_simul_plot.R
+R CMD BATCH --no-save lscores_plot.R
+cd ..
+```
+
+Please contact lihua.lei@berkeley.edu for further questions.
+
